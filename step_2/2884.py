51535bfd5f10704522ecd4bf93ec391a9918c757
nevcea <aduoss01@gmail.com>
2025-08-27 17:56:33 +0900

    step 2 clear

--- step_2/2884.py
+++ step_2/2884.py
@@ -1,1 +1,14 @@
-# https://www.acmicpc.net/problem/2884
+h, m = map(int, input().split(" "))
+
+if m >= 45:
+  m -= 45
+  print(h, m)
+else:
+  m += 15
+  
+  if h == 0:
+    h = 23
+  else:
+    h -= 1
+  
+  print(h, m)
